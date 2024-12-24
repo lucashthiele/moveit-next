b7323b3ad8e08f24ddd9529453aad953ef2c7c66
lucashthiele <lucasthiele0@outlook.com>
2024-12-24 12:07:00 -0300

    refactor: move icon from head to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/global.css";
 
 export const metadata: Metadata = {
   title: "Inicio | GetMoved",
+  icons: [{ rel: "shortcut icon", url: "favicon.png" }],
 };
 export default function HomeLayout({
   children,
@@ -12,8 +13,6 @@ export default function HomeLayout({
   return (
     <html>
       <head>
-        <link rel="shortcut icon" href="favicon.png" type="image/png" />
-
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
